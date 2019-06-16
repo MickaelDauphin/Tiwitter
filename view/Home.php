d37b20318ac98f810853cac0b21b887b8dba2d73
Mickael <mickou43000@hotmail.fr>
2019-06-16 15:41:22 +0200

    Debut css home

--- view/Home.php
+++ view/Home.php
@@ -4,17 +4,15 @@
    <title>Tiwitter</title>
    <meta charset="utf-8" />
    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-   <!--<link rel="stylesheet" href="cssTest.css" />-->
    <link rel="stylesheet" href="../css/Home.css" />
-   <!--<link rel="icon" href="../images/logo.ico"/>
-   <script src="../js/states.js"></script>-->
+
 </head>
 
 <body>
-	<h1>Tiwitter</h1>
+    <div class="centered wrapper"><h1>Tiwitter</h1></div>
 	<!--<img id="logo" src="../images/logo.png" alt="image" title="icon_login"/>-->
 
-    <h2>Identité : <a id="Name" href="/user"><?php echo  $params['app']->getSessionParameters('user')['username']; ?></a></h2>
+    <div class="centered wrapper"><h2>Identité : <a id="Name" href="/user"><?php echo  $params['app']->getSessionParameters('user')['username']; ?></a></h2></div>
 
     <!--
 	<img src="../images/TVr.png" id="TV" onclick="changeState('TV', ['TVr.png', 'TVg.png'])"/>
@@ -22,8 +20,14 @@
     <script type="text/javascript">
         var objectList = <?php echo $params['objectList'] ?>;
     </script>
-    <textarea type="text" size="140" rows="4" cols="100" maxlength="140" placeholder="Entrez un texte à tiwiter (140 caractères max)" name="tiwit" required></textarea>
-    <button type="tiwiter">Tiwiter</button>
+    <div class="group">
+        <textarea type="text" size="140" rows="4" cols="100" maxlength="140" name="tiwit" required></textarea>
+        <span class="highlight"></span><span class="bar"></span>
+        <label>Entrez un texte à tiwiter (140 caractères max)</label>
+    </div>
+    <form action="/home/tiwit">
+    <button class="button">Tiwiter</button>
+    </form>
     <!--<img src="../images/TVr.png" id="TV" onclick="changeState('TV', ['TVr.png', 'TVg.png'], objectList[0]);" />
 	<img src="../images/PCr.png" id="PC" onclick="changeState('PC', ['PCr.png', 'PCg.png'], objectList[1]);"/>
 
@@ -35,7 +39,7 @@
 <footer>
     <form action="/">
         <br/>
-        <button type="disconnect">Se déconnecter</button>
+        <button class="button">Se déconnecter</button>
     </form>
 </footer>
 </html>
