66fe2597c3339cb5478e18d29ace7997dce3a50d
Mickael <mickou43000@hotmail.fr>
2019-06-17 15:11:14 +0200

    Revert "Renommage fichiers pour serveur"
    This reverts commit 6a925a096d912743ac2f0549f58b8f02301cd61e.

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace web;
+
+use App\Src\Autoloader;
+
+require_once __DIR__ . '/../app/src/Autoloader.php';
+Autoloader::register();
+
+$app = require_once __DIR__ . '/../app/Bootstrap.php';
+$app->run();
